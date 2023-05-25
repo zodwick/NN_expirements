40565cd1fc5082021cd9b757f812b5189aa717c8
zodwick <115868723+zodwick@users.noreply.github.com>
2023-05-25 21:31:18 +0530

    first base

--- micrograd/main.py
+++ micrograd/main.py
@@ -1,1 +1,11 @@
-print("hello")
+class Value:
+    def __init__(self,data):
+        self.data=data
+        
+    def __repr__(self) -> str:
+        return f"Value({self.data})"
+    
+
+
+a=Value(1)
+print(a)
