66d0aa706cefb8cc1c57f40fff3bffb1a412b177
zodwick <115868723+zodwick@users.noreply.github.com>
2023-05-25 21:37:30 +0530

    1st

--- micrograd/main.py
+++ micrograd/main.py
@@ -5,6 +5,19 @@ class Value:
     def __repr__(self) -> str:
         return f"Value({self.data})"
     
+    def __add__(self,second):
+        return Value(self.data+second.data)
+    
+    def __mul__(self,second):
+        return Value(self.data*second.data)
+    
+    def __pow__(self,second):
+        return Value(self.data**second.data)
+    
+    def __sub__(self,second):
+        return Value(self.data-second.data)
+    
+    
 
 
 a=Value(1)
