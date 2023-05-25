ffec50eeb7759bc887350a14e3a9134503201fd0
zodwick <115868723+zodwick@users.noreply.github.com>
2023-05-25 21:44:48 +0530

    dsa

--- micrograd/main.py
+++ micrograd/main.py
@@ -1,24 +1,32 @@
 class Value:
-    def __init__(self,data):
+    def __init__(self,data,children=(),op=""):
+        
+        # if list then multiple pointers to same child may occur : not needed???
+        
         self.data=data
+        self._prev=set(children)
+        self._op=op
         
     def __repr__(self) -> str:
         return f"Value({self.data})"
     
     def __add__(self,second):
-        return Value(self.data+second.data)
+        return Value(self.data+second.data,(self,second),"+")
     
     def __mul__(self,second):
-        return Value(self.data*second.data)
+        return Value(self.data*second.data,(self,second),"*")
     
     def __pow__(self,second):
-        return Value(self.data**second.data)
+        return Value(self.data**second.data,(self,second),"**")
     
     def __sub__(self,second):
-        return Value(self.data-second.data)
+        return Value(self.data-second.data, (self,second),"-")
     
     
 
 
-a=Value(1)
-print(a)
+a=Value(4)
+b=Value(-5)
+c=Value(6)
+d=a+a
+print(d._prev)
