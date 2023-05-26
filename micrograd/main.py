45cedc4d6b57929f737ff99a65f6e2ab25d788aa
zodwick <115868723+zodwick@users.noreply.github.com>
2023-05-27 01:25:32 +0530

    tanh

--- micrograd/main.py
+++ micrograd/main.py
@@ -1,6 +1,7 @@
 
 from visualiser import draw_dot
 from graphviz import Source,Digraph
+import math
 
 class Value:
     def __init__(self,data,_children=(),op="",label=""):
@@ -27,16 +28,35 @@ class Value:
     def __sub__(self,second):
         return Value(self.data-second.data, (self,second),"-")
     
-    
+    def tanh(self):
+        x=self.data
+        t=(math.exp(2*x)-1)/( math.exp(2*x)+1)
+        out=Value(t,(self,),"tanh")
+        return out
 
+#input nodes
+x1=Value(3.0,label="x1")
+x2=Value(4.0,label="x2")
 
-a=Value(4)
-b=Value(-5)
-c=Value(6)
-d=a+a
-# dot_representation=(draw_dot(d))
-# dot = Digraph(format='png')
-# dot.engine = 'dot'
-# dot.body.append(dot_representation)
+#weights
+w1=Value(-3.0,label="w1")
+w2=Value(5.0,label="w2")
 
-draw_dot(d)
+# bias of neuron
+b=Value(8.0,label="b")
+
+# x1*w1+x2*w2+b (sigma xi*wi+b) where b is bias
+
+x1w1=x1*w1
+x1w1.label="x1w1"
+
+x2w2=x2*w2
+x2w2.label="x2w2"
+
+x1w1x2w2=x1w1+x2w2
+x1w1x2w2.label="x1w1x2w2"
+
+n=x1w1x2w2+b
+n.label="n"
+o=n.tanh()
+draw_dot(o)
