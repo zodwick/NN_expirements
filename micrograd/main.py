eaf6b052338c555254786bcf1e18b57db68f6718
zodwick <115868723+zodwick@users.noreply.github.com>
2023-05-27 02:12:31 +0530

    lessggo

--- micrograd/main.py
+++ micrograd/main.py
@@ -9,7 +9,9 @@ class Value:
         # if list then multiple pointers to same child may occur : not needed???
         
         self.data=data
+        self._gradient=lambda:None
         self._prev=set(_children)
+        self.grad=0
         self._op=op
         self.label=label
         
@@ -17,33 +19,51 @@ class Value:
         return f"Value({self.data})"
     
     def __add__(self,second):
-        return Value(self.data+second.data,(self,second),"+")
+        out= Value(self.data+second.data,(self,second),"+")
+        def _gradient():
+            self.grad=1*out.grad
+            second.grad=1*out.grad
+            
+        out._gradient=_gradient
+        #calling function to calculate gradient of output wrt self and second storing in grad attribute of self and second
+        return out
     
     def __mul__(self,second):
-        return Value(self.data*second.data,(self,second),"*")
+        out= Value(self.data*second.data,(self,second),"*")
+        def _gradient():
+            self.grad=second.data*out.grad
+            second.grad=self.data*out.grad
+        out._gradient=_gradient
+        return out   
+    # def __pow__(self,second):
+    #     return Value(self.data**second.data,(self,second),"**")
     
-    def __pow__(self,second):
-        return Value(self.data**second.data,(self,second),"**")
-    
-    def __sub__(self,second):
-        return Value(self.data-second.data, (self,second),"-")
+    # def __sub__(self,second):
+    #     return Value(self.data-second.data, (self,second),"-")
+
     
     def tanh(self):
+        
         x=self.data
         t=(math.exp(2*x)-1)/( math.exp(2*x)+1)
         out=Value(t,(self,),"tanh")
+        def _gradient():
+            self.grad=(1-t**2)*out.grad
+            
+        out._gradient=_gradient
         return out
 
+
 #input nodes
-x1=Value(3.0,label="x1")
-x2=Value(4.0,label="x2")
+x1=Value(2.0,label="x1")
+x2=Value(0.0,label="x2")
 
 #weights
 w1=Value(-3.0,label="w1")
-w2=Value(5.0,label="w2")
+w2=Value(1.0,label="w2")
 
 # bias of neuron
-b=Value(8.0,label="b")
+b=Value(6.88137,label="b")
 
 # x1*w1+x2*w2+b (sigma xi*wi+b) where b is bias
 
@@ -59,4 +79,8 @@ x1w1x2w2.label="x1w1x2w2"
 n=x1w1x2w2+b
 n.label="n"
 o=n.tanh()
+o.label="o"
+o.grad=1
+o._gradient()
+
 draw_dot(o)
