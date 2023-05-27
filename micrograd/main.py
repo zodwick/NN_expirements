fbea2742c04cbd30ac5f29894ad50fcb7399a608
zodwick <115868723+zodwick@users.noreply.github.com>
2023-05-27 17:57:22 +0530

    cleanup

--- micrograd/main.py
+++ micrograd/main.py
@@ -49,7 +49,6 @@ class Value:
 
     def __mul__(self, other):
 
-
         if not isinstance(other, Value):
             other = Value(other)
         out = Value(self.data*other.data, (self, other), "*")
@@ -64,7 +63,7 @@ class Value:
 
         # a is self and 2 is other
         return self*other
-    
+
     def __radd__(self, other):
         return self+other
 
@@ -73,19 +72,16 @@ class Value:
         self.grad = 1
         visited = set()
 
-
         def build_backprop_list(x: Value):
             for children in x._prev:
                 if children not in visited:
                     visited.add(children)
                     build_backprop_list(children)
             topo_list.append(x)
-            
 
         build_backprop_list(self)
 
         for node in reversed(topo_list):
-            print(node.label,"============", node.data)
             node._gradient()
 
     def tanh(self):
@@ -105,37 +101,30 @@ class Value:
     def exp(self):
         x = self.data
         out = Value(math.exp(x), (self, ), 'exp')
-        
+
         def _backward():
-            # print("out.grad",out.grad)
-            # print("out.data",out.data)
-            # print("self grad",self.grad)
-            # print("output",out.data*out.grad)
-            self.grad += out.data * out.grad 
+            self.grad += out.data * out.grad
         out._gradient = _backward
-            
+
         return out
 
     def __pow__(self, other):
-        assert isinstance(other, (int, float))
+        assert isinstance(other, (int, float)) # only supporting x^k
         out = Value(self.data**other, (self,), f"**{other}")
 
         def _gradient():
-            # local_gradient = other*(self.data**(other-1))
-            # self.grad += local_gradient*out.grad
-            self.grad += other * (self.data ** (other - 1)) * out.grad
-
+            local_gradient = other*(self.data**(other-1))
+            self.grad += local_gradient*out.grad
 
         out._gradient = _gradient
         return out
-    
-    def __neg__(self): # -self
+
+    def __neg__(self):  # -self
         return self * -1
 
     def __truediv__(self, other):
         # a/b = a*(b**-1)
         return self*other**-1
-
 
 
 # inputs x1,x2
@@ -147,17 +136,17 @@ w2 = Value(1.0, label='w2')
 # bias of the neuron
 b = Value(6.8813735870195432, label='b')
 # x1*w1 + x2*w2 + b
-x1w1 = x1*w1; x1w1.label = 'x1*w1'
-x2w2 = x2*w2; x2w2.label = 'x2*w2'
-x1w1x2w2 = x1w1 + x2w2; x1w1x2w2.label = 'x1*w1 + x2*w2'
-n = x1w1x2w2 + b; n.label = 'n'
+x1w1 = x1*w1
+x1w1.label = 'x1*w1'
+x2w2 = x2*w2
+x2w2.label = 'x2*w2'
+x1w1x2w2 = x1w1 + x2w2
+x1w1x2w2.label = 'x1*w1 + x2*w2'
+n = x1w1x2w2 + b
+n.label = 'n'
 # ----
 e = (2*n).exp()
 o = (e - 1) / (e + 1)
 # ----
 o.label = 'o'
 o.backpropogate()
-
-
-
-
